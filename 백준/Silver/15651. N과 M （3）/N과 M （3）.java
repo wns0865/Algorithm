84d26c289f38c3e152848c883968e4f2b04d1c6b
wns0865 <wns0865@naver.com>
2025-01-11 23:51:26 +0900

    Update N과 M （3）.java

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,44 +1,30 @@
-
 import java.util.*;
 import java.io.*;
 public class Main {
-	
-	static int n;
-	static int m;
-	static int[]  arr;
-	static boolean [] check;
-	public static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
+	static int n ,m,arr[];
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n=Integer.parseInt(st.nextToken());
 		m=Integer.parseInt(st.nextToken());
-	
-		arr=new int[n];
-		check= new boolean[n];
-		
-		dfs(0);
+		arr= new int[m];
+		dfs(0,0);
 		System.out.println(sb);
+		
 	}
 
-	public static void dfs (int idx){
-		
-		if(idx==m) {
+	private static void dfs(int start, int count) {
+		if(count>=m) {
 			for(int i=0;i<m;i++) {
-			sb.append(arr[i]).append(" ");
-		}
-			sb.append("\n");
+				sb.append(arr[i]+" ");
+			}sb.append("\n");
 			return;
 		}
-		else {
 		for(int i=0;i<n;i++) {
-			
-			arr[idx]=i+1;
-			check[i]=true;
-			dfs(idx+1);
-			check[i]=false;
-			}
+				arr[count]=i+1;
+				dfs(i+1,count+1);
 		}
+		
 	}
 }
