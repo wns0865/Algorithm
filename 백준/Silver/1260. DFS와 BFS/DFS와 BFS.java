218216a902120bcb43f596bd7195ec4e664cc127
wns0865 <wns0865@naver.com>
2025-01-20 20:01:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 260 ms, Memory: 20548 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,59 +1,69 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-	static int n,m,v,arr[][];
+	static int n,m,v;
 	static boolean isVisited[];
-	public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	n=sc.nextInt();
-	m=sc.nextInt();
-	v=sc.nextInt();
-	isVisited=new boolean[n+1];
-	arr=new int[1001][1001];
-	for(int i=0;i<m;i++) {
-		int x=sc.nextInt();
-		int y=sc.nextInt();
-		arr[x][y]=arr[y][x]=1;
-	}
-	dfs(v);
-	isVisited=new boolean[n+1];
-	System.out.println();
-	bfs(v);
-	
-	
-	
-}
-	static void dfs (int node) {
-	isVisited[node]=true;
-	System.out.print(node +" ");
-	for(int i=1;i<n+1;i++) {
-		if(!(isVisited[i])&&arr[node][i]==1)
-		{
-			dfs(i);
+	static ArrayList <Integer> B,D;
+	static ArrayList <Integer> [] list;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
+		v=Integer.parseInt(st.nextToken());
+		B=new ArrayList<>();
+		D=new ArrayList<>();
+		list= new ArrayList[n+1];
+		for(int i=1;i<n+1;i++) {
+			list[i]=new ArrayList<>();
 		}
-	}
-	}
-	
-	static void bfs(int node) {
-		Queue<Integer> q =new ArrayDeque<>();
-		q.offer(node);
-		isVisited[node]=true;
-		System.out.print(node+" ");
-		while(!q.isEmpty())
-		{
-			int tmp=q.poll();
-			for(int i=1;i<n+1;i++) {
-			if(arr[tmp][i]==1&&!(isVisited[i])) {
-				isVisited[i]=true;
-				q.offer(i);
-				System.out.print(i+" ");
-			}
-			}
-			
+		int a=0;int b=0;
+		for(int i=0;i<m;i++) {
+			st = new StringTokenizer(br.readLine());
+			a=Integer.parseInt(st.nextToken());
+			b=Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
 		}
 		
+		 isVisited = new boolean[n+1];
+		dfs(v,0);
+		 isVisited = new boolean[n+1];
+		bfs(v,0);
+		for(int i=0;i<D.size();i++)System.out.print(D.get(i)+" ");
+		System.out.println();
+		for(int i=0;i<B.size();i++)System.out.print(B.get(i)+" ");
+	}
+	static void dfs(int num, int idx) {
+		D.add(num);
+		isVisited[num]=true;
+		Collections.sort(list[num]);
+		int size=list[num].size();
+		for(int i=0;i<size;i++) {
+			int tmp=list[num].get(i);
+			if(isVisited[tmp]) continue;
+			isVisited[tmp]=true;
+			dfs(tmp,idx);
+		}
+		
+	}
+	
+	static void bfs(int num, int idx) {
+		Queue<Integer> q = new ArrayDeque<>();
+		isVisited[num]=true;
+		q.add(num);
+		B.add(num);
+		while(!q.isEmpty()) {
+			int p = q.poll();
+			Collections.sort(list[p]);
+			int size=list[p].size();
+			for(int i=0;i<size;i++) {
+				int tmp =list[p].get(i);
+				if(isVisited[tmp])continue;
+				isVisited[tmp]=true;
+				B.add(tmp);
+				q.add(tmp);
+			}
+		}
 	}
 }
